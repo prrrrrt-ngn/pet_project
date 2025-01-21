62f6aa5b91c8f285a95863ca0510e8d2c8144ed8
Анатолий Портнягин <tolikportnyagin@yandex.ru>
2025-01-21 20:07:20 +0500

    Добавлена корзина/заказ

--- app/mainmodule/controllers.py
+++ app/mainmodule/controllers.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, request, g, redirect, url_for, session
 from .models import get_db_connection
-from .views import products_view, order_view, not_found_view, carts_view
+from .views import products_view, not_found_view, cart_view
 from app.services.telegram_service import send_to_telegram
 
 main_module = Blueprint('mainmodule', __name__, template_folder='../templates')
+
 
 @main_module.before_request
 def conn():
     g.connection = get_db_connection()
     g.cursor = g.connection.cursor()
 #соединение с бд
+
 
 @main_module.after_request
 def disconn(responce):
@@ -20,28 +22,17 @@ def disconn(responce):
     return responce
 #закрытие соединения с бд
 
+
 @main_module.route('/', methods=['GET'])
 def main():
     return redirect(url_for('mainmodule.products', category_id=1))
 #редирект на страницу 1 страницу меню, здесь лучше заменить в будущем на главную страницу
 
+
 @main_module.route('/products/category/<string:category_id>', methods=["GET"])
 def products(category_id):
     return products_view(category_id)
 #контроллер отображения страницы меню (айди категории передается с url в функцию)
-
-
-# @main_module.route('/order', methods=["POST"])
-# def order():
-#     order = {}
-#     for cocktail_name, details in request.form.items():
-#         quantity = int(details)
-#         if quantity > 0:
-#             order[cocktail_name] = quantity
-    
-#     send_to_telegram(order)
-#     return order_view()
-#обработчик пост запроса для вывода в тг
 
 
 @main_module.route('/order', methods=["POST"])
@@ -60,9 +51,19 @@ def order():
     return redirect(url_for('mainmodule.products', category_id=1))
 
 
-@main_module.route('/carts', methods=["GET"])
+@main_module.route('/cart', methods=["GET", "POST"])
 def cart():
-    return carts_view()
+    if request.method == "POST":
+        product_to_remove = request.form.get('product')
+        if product_to_remove and 'cart' in session:
+            for item in session['cart']:
+                if product_to_remove in item:
+                    del item[product_to_remove]
+                    session.modified = True
+                    break
+        return redirect(url_for('mainmodule.cart'))
+    
+    return cart_view(session.get('cart', {}))
 
 
 @main_module.errorhandler(404)
--- app/mainmodule/views.py
+++ app/mainmodule/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session
+from flask import render_template
 from .models import get_products, get_categories
 
 def products_view(category_id):
@@ -6,11 +6,8 @@ def products_view(category_id):
     products_dict = get_products(category_id)
     return render_template('mainmodule/index.html', products_by_subcategories=products_dict, categories=categories)
 
-def order_view():
-    return f'Ваш заказ отправлен на бар!'
-
-def carts_view():
-    return f'{session.get['cart']}'
+def cart_view(cart):
+    return render_template('mainmodule/cart.html', cart = cart)
 
 def not_found_view():
     return render_template('404.html'), 404
